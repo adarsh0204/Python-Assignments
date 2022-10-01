a88a11bc66b49d6d9a12a2a836d268d22405f0c9
Adarsh Verma <112921040+adarsh0204@users.noreply.github.com>
2022-10-01 19:51:02 +0530

    Add files via upload

---
+++ Assignment_5.py
@@ -0,0 +1,63 @@
+                                #Assignment 5
+
+#Q1: Write a python script to remove the last digit from a given number. (for example, if
+#user enters 2534 then your output should be 253)
+'''
+a=int(input("Enter a number: "))
+print(a//10)'''
+
+#Q2: Write a python script to get the last digit from a given number. (for example, if user
+#enters 2089 then your output should be 9)
+'''
+a=int(input("Enter a number: "))
+print(a%10)'''
+
+#Q3: Write a python script to swap data of two variables.
+'''
+a=5
+b=9
+c=a
+a=b
+b=c
+print("a: ",a,"b: ",b)'''
+
+#Q4: Write a python script to find x power y, where values of x and y are given by user.
+'''
+x=int(input("Enter a number: "))
+y=int(input("Enter its power: "))
+print(x**y)'''
+
+#Q5: Write a python script which takes a three digit number from the user and displays
+#only its first digit.
+'''
+x=int(input("Enter three digit number: "))
+print(x//100)'''
+
+#Q6: Write a python script which takes a three digit number from the user and displays
+#only its middle digit.
+'''
+x=int(input("Enter three digit number: "))
+print(x//10%10)'''
+
+#Q7: Write a python script which takes a three digit number from the user and displays
+#only its last digit.
+'''
+x=int(input("Enter three digit number: "))
+print(x%10)'''
+
+#Q8: Write a python script to use IN operator to display the data present in the list.
+'''
+l1=[12,30,5,"adarsh",5.9]
+print("adarsh" in l1)'''
+
+#Q9: Write a python script to use NOT IN operator to display the data not present in list.
+'''
+l2=[12,30,5,"adarsh",5.9]
+print("adarsh" not in l2)'''
+
+#Q10: Write a python script to use IS operator to display if both variables are the same
+#object or not?
+'''
+a="adarsh"
+b=7
+print(a is b)'''
