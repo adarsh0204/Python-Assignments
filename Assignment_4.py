f40ffda2a074a764b3c751f7e76f8d5621468ff5
Adarsh Verma <112921040+adarsh0204@users.noreply.github.com>
2022-10-01 19:06:33 +0530

    Add files via upload

---
+++ Assignment_4.py
@@ -0,0 +1,61 @@
+                                #Assignment 4
+
+#Q1: Write a python script to take your name as input from the user and then print it.
+'''
+a=input("Enter Your Name: ")
+print(a)'''         
+
+#Q2: Write a python script which takes two numbers from the user, then calculate their sum
+#and display the result.
+'''
+a=int(input("Enter no: "))
+b=int(input("Enter no: "))
+print(a+b)'''
+
+#Q3: Write a python script which takes the radius from the user and display area of a circle.
+#area of circle=pi*r**2
+'''
+r=float(input("Enter radius: "))
+print("Area of circle is:",(3.14*r**2))'''
+
+#Q4: Write a python script to calculate the square of a number. Number is entered by the user.
+'''
+n=int(input("Enter a number: "))
+print(n**2)'''
+
+#Q5: Write a python script to calculate the area of Triangle. Number is entered by the user.
+#area of triangle= 1/2*b*h
+'''
+b=float(input("Enter base: "))
+h=float(input("Enter height: "))
+print("Area of triangle is: ",1/2*b*h)'''
+
+#Q6: Write a python script to calculate average of three numbers, entered by the user.
+'''
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+print("Average of three no: ",(a+b+c)/3)'''
+
+#Q7: Write a python script to calculate simple interest.
+#simple interest= (p*r*t)/100
+'''
+p=float(input("Enter Principle: "))
+r=float(input("Enter Rate: "))
+t=float(input("Enter Time in years: "))
+print("Simple Intrest is: ",(p*r*t)/100)'''
+
+#Q8: Write a python script to calculate the volume of a cuboid.
+#volume of cuboid=l*b*h
+'''
+l=float(input("Enter length: "))
+b=float(input("Enter breadth: "))
+h=float(input("Enter height: "))
+print("volume of cuboid: ",l*b*h)'''
+
+#Q9: Write a python script to calculate area of a rectangle.
+#area of rectangle= l*w
+'''
+l=float(input("Enter length: "))
+w=float(input("Enter width: "))
+print("Area of rectangle: ",l*w)'''
