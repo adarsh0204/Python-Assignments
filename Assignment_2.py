29fe7a172d2165eb6bacc857334a1b991e5c37f3
Adarsh Verma <112921040+adarsh0204@users.noreply.github.com>
2022-11-28 21:41:34 +0530

    Update Assignment_2.py

--- Assignment_2.py
+++ Assignment_2.py
@@ -1,5 +1,4 @@
                                             #Assignment 2
-
 #Q1: Write a python script to print types of variables. Create 5 variables each of them
 #containing different types of data. (like 35, True, “MySirG”,5.46, 3+4j, etc)
 '''
